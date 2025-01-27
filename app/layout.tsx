9c3eb0e87063b4aa6522fc758a0e6559952b95ac
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-01-27 10:32:26 -0300

    Adicionando título 'leiturece' nas páginas

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <title>Leiturece</title>
         {children}
       </body>
     </html>
